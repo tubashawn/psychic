d0c5ae91a76d679d5d2d8e9fe983ceabb171fe85
tubaShawn <shawnrmangum@gmail.com>
2018-05-05 22:22:35 -0600

    almost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,35 +2,45 @@ var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
     "T", "U", "V", "W", "X", "Y", "Z"
 ];
 var secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
- console.log(secretLetter);
+console.log(secretLetter);
 var win = 0;
 var lose = 0;
 var remains = 10;
 var letter = [];
 
+function loop() {
+    for (var i = 0; i < letter.length; i++) {
+        document.getElementById("myLetters").textContent = ((i+1) + ": " + letter[i]);
+    }
+}
 
- document.onkeyup = function (event) {
+document.onkeyup = function (event) {
     var press = event.key;
     var big = press.toUpperCase();
+
+    
+
+    loop();
     document.getElementById("guess").textContent = big;
-    if(big == secretLetter) {
+    letter.push(big);
+    console.log(letter);
+
+    if (big == secretLetter) {
         alert("Congratulations! You got it right!");
         document.getElementById("won").textContent = win += 1;
         var newSecret = alphabet[Math.floor(Math.random() * alphabet.length)];
         secretLetter = newSecret;
         console.log(secretLetter);
         document.getElementById("remaining").textContent = 10;
-        lose = 10;
-        } else {
-            alert("Not quite, try again!");
-            document.getElementById("remaining").textContent = remains -=1;
-            if(remains == 0) {
-                alert("You are out of guesses. Better luck next time!");
-                document.getElementById("lost").textContent = lose  += 1;
-                document.getElementById("remaining").textContent = 10;
-                lose = 10;
-            }
+        remains = 10;
+    } else {
+        alert("Not quite, try again!");
+        document.getElementById("remaining").textContent = remains -= 1;
+        if (remains == 0) {
+            alert("You are out of guesses. Better luck next time!");
+            document.getElementById("lost").textContent = lose += 1;
+            document.getElementById("remaining").textContent = 10;
+            remains = 10;
         }
-    };
-
-
+    }
+};
